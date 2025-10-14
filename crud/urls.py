9b20aa65a8c09288af509c125064403176353406
FELIPE LOPES CEZARIO <felipe.cezario@aluno.senac.rio>
2025-10-13 21:13:50 -0300

    Primeiro comit - crud

---
+++ crud/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import home, salvar, delete
+
+urlpatterns = [
+    path('', home),
+    path('salvar/', salvar, name = "salvar"),
+    path('apagar/<int:id>', delete, name = "delete"),
+
+]
---
+++ crud/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect,get_object_or_404
+from .models import Pessoa
+
+
+
+# Create your views here.
+
+def home(request):
+    pessoas = Pessoa.objects.all()
+    return render(request,"index.html",{"pessoas":pessoas})
+
+def salvar(request):
+    vnome = request.POST.get("nome")
+    vidade = request.POST.get("idade")
+    Pessoa.objects.create(nome=vnome,idade=vidade)
+    pessoas = Pessoa.objects.all()
+    return render(request,"index.html",{"pessoas":pessoas})
+
+def delete(request, id):
+    pessoa = Pessoa.objects.get(id = id)
+    pessoa.delete()
+    return redirect(home)
+
+
+
+
+
+
